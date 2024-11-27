a374f50d37285b53cb75ab3a61689c2020bf6e7d
ar0nK <kosaa@kkszki.hu>
2024-11-27 12:23:19 +0100

    Co-authored-by: szjzsf <szjzsf@users.noreply.github.com>

--- Event_Flow/app.js
+++ Event_Flow/app.js
@@ -59,3 +59,24 @@ document.getElementById('createEventBtn').addEventListener('click', function() {
   counter.init();
 
 })();
+
+
+
+//események megjelenítése a jsonból
+/*fetch("./database.json").then(response => response.json()).then(data => {
+  console.log(data);
+  data.map(function(event) {
+    document.getElementById('eventCards').innerHTML += `
+    <div class="col-md-3 event-card">
+              <div class="card">
+                  <div class="event-image">
+                  <img src="${imgSrc} " class="kartya-kep card-img-top" /> <!-- Use the image source from the input -->
+                  </div>
+                  <div class="card-body">
+                      <h5 class="card-title">${name}</h5>
+                      <p class="card-text">Helyszín: ${location}</p>
+                      <p class="d-inline-block text-truncate card-text" style="max-width: 275px;">${desc}</p>
+                      <p class="card-text"><small class="text-muted">${isPrivate ? 'Privát esemény' : 'Nyilvános esemény'}</small></p>
+                  </div>
+              </div>
+          </div>`})});*/
